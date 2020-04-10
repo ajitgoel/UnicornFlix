4752d8fafadcb54e2a9c70bc887d67e252358cdf
ajitgoel@gmail.com <ajitgoel@gmail.com>
2020-04-10 16:23:21 -0500

    added genre, added actors(not working completely)

--- src/components/Admin/index.jsx
+++ src/components/Admin/index.jsx
@@ -8,39 +8,41 @@ import PopoverProgress from '../PopoverProgress';
 // Insert Location 2
 import { withAuthenticator } from 'aws-amplify-react';
 // Insert Location 4
-import Amplify, {
-  Auth, API, graphqlOperation, Storage,
-} from 'aws-amplify';
+import Amplify, {Auth, API, graphqlOperation, Storage,} from 'aws-amplify';
 import awsvideoconfig from '../../aws-video-exports';
 import { createVodAsset, createVideoObject } from '../../graphql/mutations';
+import CreatableSelect from 'react-select/creatable';
 
-class Admin extends React.Component {
-  constructor(props) {
+class Admin extends React.Component 
+{  
+  constructor(props) 
+  {
     super(props);
     this.state = {
-      titleVal: '', descVal: '', groups: [], progress: 0, genreVal: ''		
-    };
+      titleVal: '', 
+      genreVal: '', 
+      actors: [],
+      descVal: '', 
+      groups: [], 
+      progress: 0};
     this.submitFormHandler = this.submitFormHandler.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.createAdminPanel = this.createAdminPanel.bind(this);
   }
 
-  componentDidMount() {
+  componentDidMount() 
+  {
     // Insert Location 5
     const region = Amplify._config.aws_project_region;
-    Auth.currentSession()
-      .then((data) => {
-        const groups = data.idToken.payload['cognito:groups'];
-        if (groups) {
-          this.setState({ groups: data.idToken.payload['cognito:groups'] });
-        }
-      });
-    Storage.configure({
-      AWSS3: {
-        bucket: awsvideoconfig.awsInputVideo,
-        region,
-      },
-    });  
+    Auth.currentSession().then((data) => 
+    {
+      const groups = data.idToken.payload['cognito:groups'];
+      if (groups) 
+      {
+        this.setState({ groups: data.idToken.payload['cognito:groups'] });
+      }
+    });
+    Storage.configure({AWSS3: {bucket: awsvideoconfig.awsInputVideo,region,},});  
   }
 
   myCallback = (dataFromChild) => {
@@ -62,58 +64,79 @@ class Admin extends React.Component {
     this.setState({ descVal: event.target.value });
   }
 
-
   submitFormHandler(event) {
     event.preventDefault();
     // Insert Location 6
     const uuid = uuidv4();
     const adminPanel = this;
-    const videoObject = {
-      input: {
-        id: uuid,
-      },
-    };
-
-    API.graphql(graphqlOperation(createVideoObject, videoObject)).then((response, error) => {
-      if (error === undefined) {
-        const {
-          titleVal, descVal, file, fileName,genreVal,
-        } = this.state;
+    const videoObject = {input: {id: uuid,},};
+    API.graphql(graphqlOperation(createVideoObject, videoObject)).then((response, error) => 
+    {
+      if (error === undefined) 
+      {
+        const {titleVal, descVal, file, fileName,genreVal, actors} = this.state;
         const fileExtension = fileName.toLowerCase().split('.');
-        const videoAsset = {
-          input: {
+        var actorsToBeSaved = [];
+        var length = actors.length;
+        for (var i = 0; i < length; i++) 
+        {
+          actorsToBeSaved.push(actors[i].value);
+        }
+        
+        const videoAsset = 
+        {
+          input: 
+          {
             title: titleVal,
             description: descVal,
             vodAssetVideoId: uuid,	
+            actors:actorsToBeSaved, 
             genre: genreVal,
           },
         };
         API.graphql(graphqlOperation(createVodAsset, videoAsset));
-        Storage.put(`${uuid}.${fileExtension[fileExtension.length - 1]}`, file, {
-          progressCallback(progress) {
+        Storage.put(`${uuid}.${fileExtension[fileExtension.length - 1]}`, file, 
+        {
+          progressCallback(progress) 
+          {
             const { loaded, total } = progress;
             console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
-            adminPanel.setState({
-              progress: (loaded / total) * 100,
-            });
+            adminPanel.setState({progress: (loaded / total) * 100,});
           },
           contentType: 'video/*',
         })
-          .then(() => console.log(`Successfully Uploaded: ${uuid}`))
-          .catch((err) => console.log(`Error: ${err}`));
+        .then(() => 
+          {
+            console.log(`Successfully Uploaded: ${uuid}`)
+            this.setState({ titleVal:'' });
+            this.setState({ genreVal: '' });
+            this.setState({ descVal: '' });            
+            this.setState({ actors: []});            
+          }
+          )
+        .catch((err) => console.log(`Error: ${err}`));
       }
     });
   }
+  
+  handleActorsChange = (newValue, actionMeta) => 
+  {
+    var tempActors = [];
+    var length = newValue.length;
+    for (var i = 0; i < length; i++) 
+    {
+      tempActors.push(newValue[i]);
+    }
+    this.setState({actors: tempActors});
+  };
 
-  createAdminPanel() {
-    const {
-      groups,
-      titleVal,
-      descVal,	
-      genreVal,
-      progress,
-    } = this.state;
-    if (groups.includes('Admin')) {
+  createAdminPanel() 
+  {
+    const {groups,titleVal,descVal,	genreVal,actors, progress,} = this.state;
+    const actorsOptions = [{ value: '', label: ''},];
+  
+    if (groups.includes('Admin')) 
+    {
       return (
         <div>
           <header>
@@ -125,6 +148,9 @@ class Admin extends React.Component {
                 <br />
                 <input type="text" value={genreVal} name="genreVal" placeholder="Genre" 
                 onChange={this.handleChange} />
+                <br />
+                <CreatableSelect placeholder="Actors" isMulti onChange={this.handleActorsChange} 
+                options={actorsOptions}/>
                 <br />
                 <textarea className="desTextA" rows="4" cols="50" value={descVal} name="descVal" 
                 placeholder="Description" onChange={this.handleChange} />
